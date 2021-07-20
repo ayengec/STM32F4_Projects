fa6e522024a600eac6721f4fe9bee5bd0efefc8b
Alican Yengeç <83873798+ayengec@users.noreply.github.com>
2021-07-20 14:35:35 +0300

    Update main.c

--- Low_Power_Modes/Standby_Mode/code/main.c
+++ Low_Power_Modes/Standby_Mode/code/main.c
@@ -1,3 +1,5 @@
+// ayengec STM32 Vendor Specific Standbymode Example with registers
+
 #include "stm32f407xx.h"
 #include "Delay.h"
 #include "RCC_Config.h"
@@ -13,27 +15,32 @@ void GPIOD_Config(void)
 
 void standbyModeEn(void)
 {
-	PWR->CR  |= (1<<1);			// PDDS: Power-down deepsleep 1: Enter Standby mode when the CPU enters deepsleep
-	SCB->SCR |= (3<<2);
-	// STM32 CORTEX M4 Instruction Set: System Control Block:system control register
-	// SLEEPONEXIT; 1: Enter sleep, or deep sleep, on return from an interrupt service routine.
-	// SLEEPDEEP bit;
-	__WFI();                    // Wait for interrupt. Cortex M4 instruction
-}
-int main(void)
-{
+
 	// WKUP pin is PA0
 	// Exiting Standby mode: WKUP pin rising edge, RTC alarm (Alarm A and Alarm B), RTC wakeup,
 	// tamper event, time stamp event, external reset in NRST pin, IWDG reset.
+	PWR->CR  |= 0x00000004;     // Clear WakeUP Flag
+	while(!PWR_CR_CWUF);		// Wait wakeup flag is cleared
+	PWR->CSR |= 0x00000100; 	// Enable WakeUp pin : WKUP pin is used for wakeup from Standby mode and forced in input pull down
+	    						// configuration (rising edge on WKUP pin wakes-up the system from Standby mode).
 
-	//while(!PWR_CSR_SBF);		// All registers are reset(0) after wakeup from Standby except for PWR_CR
+	//**************************** Standby configuration ****************************
+
+	// STM32 CORTEX M4 Instruction Set: System Control Block:system control register
+	// SLEEPONEXIT; 1: Enter sleep, or deep sleep, on return from an interrupt service routine.
+	// sleeponexit automatically forces to low power mode! it blocks to process multithread!
+	// SLEEPDEEP bit;    if sleepdeep ==1 Deep Sleep mode, else normal sleep mode
+	PWR->CR  |= (1<<1);			// PDDS: Power-down deepsleep 1: Enter Standby mode when the CPU enters deepsleep
+	SCB->SCR |= (1<<2)|(1<<1);	// SLEEPDEEP =1, SLEEPONEXIT=1
+	__WFI();                    // Wait for interrupt. Cortex M4 instruction, entering standby mode
+}
+int main(void)
+{
+
 	SysClockConfig();
     GPIOD_Config();
+    RCC->APB1ENR |= (1<<28);	// PWREN: Power interface clock enable
 
-    PWR->CR  |= 0x00000004;     // Clear WakeUP Flag
-    while(!PWR_CR_CWUF);		// Wait wakeup flag is cleared
-    PWR->CSR |= 0x00000100; 	// Enable WakeUp pin : WKUP pin is used for wakeup from Standby mode and forced in input pull down
-    							// configuration (rising edge on WKUP pin wakes-up the system from Standby mode).
     Delay_ms(500);
     while(1)
 	{
@@ -44,7 +51,6 @@ int main(void)
 		GPIOD->ODR = 0x00004000;				// PD14 is ON, others OFF
 		Delay_ms(700);
 
-		// SCB->SCR |= 0x00000002; 				// SLEEPONEXIT=1; SLEEPDEEP=0
 		GPIOD->ODR = 0x00002000;				// PD13 is ON, others OFF
 		Delay_ms(800);
 
